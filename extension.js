52e9288624b62c76a1e59a838dc5cfbd434309c7
Slulego <slulego@live.com>
2020-12-02 16:59:34 -0500

    Handle formatting comments

--- extension.js
+++ extension.js
@@ -11,15 +11,17 @@ function activate(context) {
 
             for (let lineIndex = 0; lineIndex < document.lineCount; lineIndex++) {
                 let line = document.lineAt(lineIndex)
+                // Remove @// or // comments from line
+                let lineText = line.text.replace(/\@?\/\/.*$/, '')
 
                 // Whitespace at the beginning of the line.
                 let spaceMatch = line.text.match(/^\s*/)
                 let spaceLength = spaceMatch ? spaceMatch[0].length : 0
 
-                if (line.text.match(/^\s*\<\/\w+\>/g)) {
+                if (lineText.match(/^\s*\<\/\w+\>/g)) {
                     // Begin in </tag>
                     indentCount--
-                } else if (line.text.match(/^\s*\}/g)) {
+                } else if (lineText.match(/^\s*\}/g)) {
                     // Begin in }
                     indentCount--
                 }
@@ -31,19 +33,19 @@ function activate(context) {
 
                 output.push(edit)
 
-                if (line.text.match(/^\s*\<\w+/g)) {
+                if (lineText.match(/^\s*\<\w+/g)) {
                     // Begin in <tag>
-                    if (!line.text.match(/\<\/\w+\>$/g)) {
-                        // Not end with </tag>
+                    if (!lineText.match(/\<\/\w+\>$|\/\>/g)) {
+                        // Not end with </tag> or />
                         indentCount++
                     }
-                } else if (line.text.match(/\<\/\w+\>$/g)) {
-                    // End with </tag>
-                    if (!line.text.match(/^\s*\<\/\w+\>/g)) {
+                } else if (lineText.match(/\<\/\w+\>$|\/\>/g)) {
+                    // End with </tag> or />
+                    if (!lineText.match(/^\s*\<\/\w+\>/g)) {
                         // Not begin with </tag>
                         indentCount--
                     }
-                } else if (line.text.match(/\{$/g)) {
+                } else if (lineText.match(/\{$/g)) {
                     // End in {
                     indentCount++
                 }
@@ -56,143 +58,3 @@ function activate(context) {
 }
 
 exports.activate = activate
-
-
-// provideDocumentFormattingEdits(document) {
-//     let output = []
-//     let indentCount = 0
-
-//     for (let lineIndex = 0; lineIndex < document.lineCount; lineIndex++) {
-
-//         let line = document.lineAt(lineIndex)
-
-//         let whitespaceLength = 0
-//         let whitespaceMatch = line.text.match(/^\s*/)
-
-//         let openBrackets = line.text.match(/<[^\/|!][^>]*[^\/]>|<(?!.*\/?>)|{/g)
-//         let closeBrackets = line.text.match(/<\/\w+>|}/g)
-
-//         if (whitespaceMatch) {
-//             whitespaceLength = whitespaceMatch[0].length
-//         }
-
-//         let open = openBrackets ? openBrackets.length : 0
-//         let close = closeBrackets ? closeBrackets.length : 0
-
-//         if (open - close < 0)
-//             indentCount += open - close
-
-//         if (indentCount < 0) indentCount = 0
-//         let start = new vscode.Position(lineIndex, 0)
-//         let end = new vscode.Position(lineIndex, whitespaceLength)
-//         let indentation = '    '.repeat(indentCount)
-//         let edit = vscode.TextEdit.replace(new vscode.Range(start, end), indentation)
-
-//         if (open - close > 0)
-//             indentCount += open - close
-
-//         output.push(edit)
-//     }
-
-//     return output
-// }
-
-
-
-/// TRY 2
-
-   // const context = {
-            //     ROOT: 0,
-            //     ATTRIBUTES: 1,
-            //     TAG: 2,
-            //     PARENTHESIS: 3,
-            //     BRACKETS: 4,
-            //     COMMENT: 5
-            // }
-
-            // let contextList = [context.ROOT]
-
-
-//// Look for </, >, ), }, *@
-                // const unindentRegex = /\<\/|\>|\)|\}|\*\@/g
-                // let unindentMatch
-
-                // while (unindentMatch = unindentRegex.exec(line.text)) {
-
-                //     let firstChar = unindentMatch[0][0]
-                //     let currentContext = contextList[contextList.length - 1]
-
-                //     switch (currentContext) {
-                //         case context.BRACKETS:
-                //             switch (unindentMatch[0]) {
-                //                 case '}':
-                //                     contextList.pop()
-                //                     indentCount--
-                //                     break;
-                //             }
-                //             break;
-                //         case context.TAG:
-                //             if(unindentMatch[0] == '</') {
-                //                 contextList.pop()
-                //                 indentCount--
-                //             }
-                //             break;
-                //     }
-                // }
-
-                // let start = new vscode.Position(lineIndex, 0)
-                // let end = new vscode.Position(lineIndex, spaceLength)
-                // let indentation = '    '.repeat(indentCount)
-                // let edit = vscode.TextEdit.replace(new vscode.Range(start, end), indentation)
-
-                // output.push(edit)
-                // console.log(contextList.toString(), 'at line', lineIndex);
-
-                // Look for <tag, >, (, {, @//, @*
-                // const indentRegex = /<\w+|\>|\(|\{|\@\/\/|\@\*/g
-                // let indentMatch
-
-                // while (indentMatch = indentRegex.exec(line.text)) {
-
-                //     let firstChar = indentMatch[0][0]
-
-                //     switch (contextList[contextList.length - 1]) {
-                //         case context.ROOT:
-                //         case context.BRACKETS:
-                //             switch (firstChar) {
-                //                 case '<':
-                //                     contextList.push(context.ATTRIBUTES)
-                //                     indentCount++
-                //                     break;
-                //                 case '{':
-                //                     contextList.push(context.BRACKETS)
-                //                     indentCount++
-                //                     break;
-                //             }
-                //             break;
-
-                //         case context.ATTRIBUTES:
-                //             switch (firstChar) {
-                //                 case '>':
-                //                     contextList.pop()
-                //                     contextList.push(context.TAG)
-                //                     break;
-                //             }
-                //             break;
-
-                //         case context.TAG:
-                //             switch (firstChar) {
-                //                 case '<':
-                //                     contextList.push(context.ATTRIBUTES)
-                //                     indentCount++
-                //                     break;
-                //                 case '{':
-                //                     contextList.push(context.BRACKETS)
-                //                     indentCount++
-                //                     break;
-
-                //             }
-                //             break;
-                //     }
-
-                // }
