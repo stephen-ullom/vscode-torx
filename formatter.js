2b0025de6c54d75fa4bac3f2400e2d153fda3a2a
Slulego <slulego@live.com>
2020-12-08 19:57:55 -0500

    Add formatter and update icon

--- formatter.js
+++ formatter.js
@@ -11,41 +11,48 @@ function activate(context) {
 
             for (let lineIndex = 0; lineIndex < document.lineCount; lineIndex++) {
                 let line = document.lineAt(lineIndex)
+
                 // Remove @// or // comments from line
-                let lineText = line.text.replace(/\@?\/\/.*$/, '')
+                let lineText = line.text.replace(/\@\/\/.*|^\s*\/\/.*/, '')
 
                 // Whitespace at the beginning of the line.
                 let spaceMatch = line.text.match(/^\s*/)
                 let spaceLength = spaceMatch ? spaceMatch[0].length : 0
 
-                if (lineText.match(/^\s*\<\/\w+\>/g)) {
+                if (lineText.match(/^\s*\<\/\w+\>/)) {
                     // Begin in </tag>
                     indentCount--
-                } else if (lineText.match(/^\s*\}/g)) {
+                } else if (lineText.match(/^\s*\}/)) {
                     // Begin in }
                     indentCount--
                 }
 
                 let start = new vscode.Position(lineIndex, 0)
                 let end = new vscode.Position(lineIndex, spaceLength)
-                let indentation = indentCount >= 0 ? '    '.repeat(indentCount) : ''
+                let indentation = ''
+
+                if (indentCount >= 0 && !line.text.match(/^\s*$/)) {
+                    // positive indent || not only whitespace
+                    indentation = '    '.repeat(indentCount)
+                }
+
                 let edit = vscode.TextEdit.replace(new vscode.Range(start, end), indentation)
 
                 output.push(edit)
 
-                if (lineText.match(/^\s*\<\w+/g)) {
+                if (lineText.match(/^\s*\<\w+/)) {
                     // Begin in <tag>
-                    if (!lineText.match(/\<\/\w+\>$|\/\>/g)) {
+                    if (!lineText.match(/\<\/\w+\>$|\/\>\s*$/)) {
                         // Not end with </tag> or />
                         indentCount++
                     }
-                } else if (lineText.match(/\<\/\w+\>$|\/\>/g)) {
+                } else if (lineText.match(/\<\/\w+\>\s*$|\/\>\s*$/)) {
                     // End with </tag> or />
-                    if (!lineText.match(/^\s*\<\/\w+\>/g)) {
+                    if (!lineText.match(/^\s*\<\/\w+\>/)) {
                         // Not begin with </tag>
                         indentCount--
                     }
-                } else if (lineText.match(/\{$/g)) {
+                } else if (lineText.match(/\{\s*$/)) {
                     // End in {
                     indentCount++
                 }
