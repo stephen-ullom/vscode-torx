603eb84b75f61cf88037f42074146253e2b1547b
Stephen <slulego@live.com>
2021-12-27 10:09:05 -0500

    Use document tab settings instead of the workspace

--- src/extension.ts
+++ src/extension.ts
@@ -13,175 +13,186 @@ enum Context {
    CloseXmlTag = 9,
 }
 
-exports.activate = function (context: vscode.ExtensionContext) {
+exports.activate = function () {
    vscode.languages.registerDocumentFormattingEditProvider("torx", {
       provideDocumentFormattingEdits(document: vscode.TextDocument) {
-         const editorConfig = vscode.workspace.getConfiguration("editor");
-         const useSpaces = editorConfig.insertSpaces === undefined ? false : Boolean(editorConfig.insertSpaces);
-         const tabSize = Number(editorConfig.tabSize);
-         const edits: vscode.TextEdit[] = [];
-         const indentString = useSpaces ? " ".repeat(tabSize) : "\t";
-         let indent = 0;
-         let commentDepth = 0;
-         for (let lineIndex = 0; lineIndex < document.lineCount; lineIndex++) {
-            let edit: vscode.TextEdit;
-            let hasTextBefore = false;
-            const line = document.lineAt(lineIndex);
-            const lineContext: Context[] = [];
-            const spaceIndex = line.firstNonWhitespaceCharacterIndex;
-            const spaceRange = new vscode.Range(
-               new vscode.Position(lineIndex, 0),
-               new vscode.Position(lineIndex, spaceIndex)
-            );
-            // If a line is empty, then remove all whitespace
-            if (line.text.match(/^\s*$/g)) {
-               edit = vscode.TextEdit.replace(spaceRange, "");
-            } else {
-               for (let charIndex = spaceIndex; charIndex < line.text.length; charIndex++) {
-                  const lineTextRemaining = line.text.substring(charIndex);
-                  const nextChar = lineTextRemaining.charAt(0);
-                  if (nextChar === "@") {
-                     if (lineTextRemaining.charAt(1) === "*") {
-                        charIndex++;
-                        commentDepth++;
-                     } else if (lineTextRemaining.charAt(1) === "/" && lineTextRemaining.charAt(2) === "/") {
-                        charIndex += lineTextRemaining.length;
-                     }
-                  } else if (commentDepth > 0) {
-                     if (nextChar === "*" && lineTextRemaining.charAt(1) === "@") {
-                        charIndex++;
-                        commentDepth--;
-                     }
-                  } else {
-                     switch (nextChar) {
-                        case "{":
-                        case "(":
-                        case "[":
-                           const pair = getMatchingPair(lineTextRemaining);
-                           if (pair) {
-                              charIndex += pair.length - 1;
-                           } else {
-                              switch (nextChar) {
-                                 case "{":
-                                    lineContext.push(Context.OpenCurlyBracket);
-                                    break;
-                                 case "(":
-                                    lineContext.push(Context.OpenRoundBracket);
-                                    break;
-                                 case "[":
-                                    lineContext.push(Context.OpenSquareBracket);
-                                    break;
-                              }
-                              break;
-                           }
-                           break;
-                        case "}":
-                        case ")":
-                        case "]":
-                           if (
-                              [Context.OpenCurlyBracket, Context.OpenRoundBracket, Context.OpenSquareBracket].indexOf(
-                                 getLast(lineContext)
-                              ) >= 0
-                           ) {
-                              lineContext.pop();
-                           } else {
-                              switch (nextChar) {
-                                 case "}":
-                                    lineContext.push(Context.CloseCurlyBracket);
-                                    break;
-                                 case ")":
-                                    lineContext.push(Context.CloseRoundBracket);
-                                    break;
-                                 case "]":
-                                    lineContext.push(Context.CloseSquareBracket);
-                                    break;
-                              }
-                           }
-                           break;
-                        case "<":
-                           const openAttributes = lineTextRemaining.match(/^<[^\!\/\s\<\>]+/);
-                           const closeTag = lineTextRemaining.match(/^<\/[^\s\<\>]+>/);
-                           if (openAttributes) {
-                              charIndex += openAttributes[0].length - 1;
-                              lineContext.push(Context.OpenXmlAttributes);
-                           } else if (closeTag) {
-                              charIndex += closeTag[0].length - 1;
-                              if (getLast(lineContext) === Context.OpenXmlTag) {
-                                 lineContext.pop();
-                              } else {
-                                 lineContext.push(Context.CloseXmlTag);
-                              }
-                           }
-                           break;
-                        case "/":
-                           if (lineTextRemaining.charAt(1) === ">") {
-                              charIndex++;
-                              if (getLast(lineContext) === Context.OpenXmlAttributes) {
-                                 lineContext.pop();
-                              } else {
-                                 lineContext.push(Context.CloseXmlAttributes);
-                              }
-                           }
-                           break;
-                        case ">":
-                           if (getLast(lineContext) === Context.OpenXmlAttributes) {
-                              lineContext.pop();
-                              lineContext.push(Context.OpenXmlTag);
-                           }
-                           break;
-                        default:
-                           if (!hasTextBefore && lineContext.length === 0) {
-                              hasTextBefore = true;
-                           }
-                           break;
-                     }
-                  }
-               }
-
-               // Current line
-               if (
-                  [Context.CloseCurlyBracket, Context.CloseRoundBracket, Context.CloseSquareBracket].indexOf(
-                     getLast(lineContext)
-                  ) >= 0
-               ) {
-                  indent === 0 ? 0 : indent--;
-               } else if (getLast(lineContext) === Context.CloseXmlTag) {
-                  if (!hasTextBefore) {
-                     indent === 0 ? 0 : indent--;
-                  }
-               }
-
-               // Apply
-               edit = vscode.TextEdit.replace(spaceRange, indentString.repeat(indent));
-            }
-            edits.push(edit);
-
-            // Next line
-            if (
-               [
-                  Context.OpenCurlyBracket,
-                  Context.OpenRoundBracket,
-                  Context.OpenSquareBracket,
-                  Context.OpenXmlAttributes,
-                  Context.OpenXmlTag,
-               ].indexOf(getLast(lineContext)) >= 0
-            ) {
-               indent++;
-            } else if (getLast(lineContext) === Context.CloseXmlAttributes) {
-               indent === 0 ? 0 : indent--;
-            } else if (getLast(lineContext) === Context.CloseXmlTag) {
-               if (hasTextBefore) {
-                  indent === 0 ? 0 : indent--;
-               }
-            }
-         }
-         return edits;
+         const activeTextEditor = vscode.window.activeTextEditor;
+         const useSpaces = Boolean(activeTextEditor.options.insertSpaces);
+         const tabSize = Number(activeTextEditor.options.tabSize);
+         return formatDocument(document, useSpaces, tabSize);
       },
    });
 };
 
 /**
+ * Provide indentation edits
+ * @param document - vscode text document
+ * @param useSpaces - indent with spaces or tabs
+ * @param tabSize - the number of spaces if spaces are enabled
+ */
+function formatDocument(document: vscode.TextDocument, useSpaces = true, tabSize = 4): vscode.TextEdit[] {
+   const edits: vscode.TextEdit[] = [];
+   const indentString = useSpaces ? " ".repeat(tabSize) : "\t";
+   let indent = 0;
+   let commentDepth = 0;
+   for (let lineIndex = 0; lineIndex < document.lineCount; lineIndex++) {
+      let edit: vscode.TextEdit;
+      let hasTextBefore = false;
+      const line = document.lineAt(lineIndex);
+      const lineContext: Context[] = [];
+      const spaceIndex = line.firstNonWhitespaceCharacterIndex;
+      const spaceRange = new vscode.Range(
+         new vscode.Position(lineIndex, 0),
+         new vscode.Position(lineIndex, spaceIndex)
+      );
+      // If a line is empty, then remove all whitespace
+      if (line.text.match(/^\s*$/g)) {
+         edit = vscode.TextEdit.replace(spaceRange, "");
+      } else {
+         for (let charIndex = spaceIndex; charIndex < line.text.length; charIndex++) {
+            const lineTextRemaining = line.text.substring(charIndex);
+            const nextChar = lineTextRemaining.charAt(0);
+            if (nextChar === "@") {
+               if (lineTextRemaining.charAt(1) === "*") {
+                  charIndex++;
+                  commentDepth++;
+               } else if (lineTextRemaining.charAt(1) === "/" && lineTextRemaining.charAt(2) === "/") {
+                  charIndex += lineTextRemaining.length;
+               }
+            } else if (commentDepth > 0) {
+               if (nextChar === "*" && lineTextRemaining.charAt(1) === "@") {
+                  charIndex++;
+                  commentDepth--;
+               }
+            } else {
+               switch (nextChar) {
+                  case "{":
+                  case "(":
+                  case "[":
+                     const pair = getMatchingPair(lineTextRemaining);
+                     if (pair) {
+                        charIndex += pair.length - 1;
+                     } else {
+                        switch (nextChar) {
+                           case "{":
+                              lineContext.push(Context.OpenCurlyBracket);
+                              break;
+                           case "(":
+                              lineContext.push(Context.OpenRoundBracket);
+                              break;
+                           case "[":
+                              lineContext.push(Context.OpenSquareBracket);
+                              break;
+                        }
+                        break;
+                     }
+                     break;
+                  case "}":
+                  case ")":
+                  case "]":
+                     if (
+                        [Context.OpenCurlyBracket, Context.OpenRoundBracket, Context.OpenSquareBracket].indexOf(
+                           getLast(lineContext)
+                        ) >= 0
+                     ) {
+                        lineContext.pop();
+                     } else {
+                        switch (nextChar) {
+                           case "}":
+                              lineContext.push(Context.CloseCurlyBracket);
+                              break;
+                           case ")":
+                              lineContext.push(Context.CloseRoundBracket);
+                              break;
+                           case "]":
+                              lineContext.push(Context.CloseSquareBracket);
+                              break;
+                        }
+                     }
+                     break;
+                  case "<":
+                     const openAttributes = lineTextRemaining.match(/^<[^\!\/\s\<\>]+/);
+                     const closeTag = lineTextRemaining.match(/^<\/[^\s\<\>]+>/);
+                     if (openAttributes) {
+                        charIndex += openAttributes[0].length - 1;
+                        lineContext.push(Context.OpenXmlAttributes);
+                     } else if (closeTag) {
+                        charIndex += closeTag[0].length - 1;
+                        if (getLast(lineContext) === Context.OpenXmlTag) {
+                           lineContext.pop();
+                        } else {
+                           lineContext.push(Context.CloseXmlTag);
+                        }
+                     }
+                     break;
+                  case "/":
+                     if (lineTextRemaining.charAt(1) === ">") {
+                        charIndex++;
+                        if (getLast(lineContext) === Context.OpenXmlAttributes) {
+                           lineContext.pop();
+                        } else {
+                           lineContext.push(Context.CloseXmlAttributes);
+                        }
+                     }
+                     break;
+                  case ">":
+                     if (getLast(lineContext) === Context.OpenXmlAttributes) {
+                        lineContext.pop();
+                        lineContext.push(Context.OpenXmlTag);
+                     }
+                     break;
+                  default:
+                     if (!hasTextBefore && lineContext.length === 0) {
+                        hasTextBefore = true;
+                     }
+                     break;
+               }
+            }
+         }
+
+         // Current line
+         if (
+            [Context.CloseCurlyBracket, Context.CloseRoundBracket, Context.CloseSquareBracket].indexOf(
+               getLast(lineContext)
+            ) >= 0
+         ) {
+            indent === 0 ? 0 : indent--;
+         } else if (getLast(lineContext) === Context.CloseXmlTag) {
+            if (!hasTextBefore) {
+               indent === 0 ? 0 : indent--;
+            }
+         }
+
+         // Apply
+         edit = vscode.TextEdit.replace(spaceRange, indentString.repeat(indent));
+      }
+      edits.push(edit);
+
+      // Next line
+      if (
+         [
+            Context.OpenCurlyBracket,
+            Context.OpenRoundBracket,
+            Context.OpenSquareBracket,
+            Context.OpenXmlAttributes,
+            Context.OpenXmlTag,
+         ].indexOf(getLast(lineContext)) >= 0
+      ) {
+         indent++;
+      } else if (getLast(lineContext) === Context.CloseXmlAttributes) {
+         indent === 0 ? 0 : indent--;
+      } else if (getLast(lineContext) === Context.CloseXmlTag) {
+         if (hasTextBefore) {
+            indent === 0 ? 0 : indent--;
+         }
+      }
+   }
+   return edits;
+}
+
+/**
  * Get the last element in array
+ * @param array - any array
  */
 function getLast(array: any[]): any {
    return array.slice(-1)[0];
@@ -189,7 +200,7 @@ function getLast(array: any[]): any {
 
 /**
  * Get the string including the matching pair
- * @param text - should begin with (, { or [
+ * @param text - a string that begins with (, { or [
  */
 function getMatchingPair(text: string): string {
    const pairs = [
@@ -238,7 +249,7 @@ function getMatchingPair(text: string): string {
 
 /**
  * Get the string including the matching quotes
- * @param text - should begin with ', " or `
+ * @param text - a string that begins with ', " or `
  */
 function getMatchingQuotes(text: string): string {
    const quotes = ["'", '"', "`"];
